a1b3c8f05ec1e18284106a3351ad34dbb082529f
cbb4bama <cbb4bama@gmail.com>
2024-05-20 11:40:53 -0500

    Scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,11 @@
 console.log("Hello world");
+function menuToggle() {
+    var x = document.getElementById('myNavtoggle');
+      var isMobileBreakpoint = window.innerWidth <= 641
+  
+    if (x.className === 'navtoggle' && isMobileBreakpoint) {
+      x.className += ' responsive';
+    } else {
+      x.className = 'navtoggle';
+    }
+  }
